539007c5e128d4a2734b0e23a716228e9c5a472e
Sung Kwang David Kim <sungkwangkim@gmail.com>
2017-01-20 11:03:34 -0800

    allow sounds on click

--- 01 - JavaScript Drum Kit/js/jquery.sounds.js
+++ 01 - JavaScript Drum Kit/js/jquery.sounds.js
@@ -4,16 +4,32 @@ $(function(){
   //   console.log($(`audio[data-key="${65}"]`).element);
   // });
   $(window).keydown((e) => {
-    console.log("keydown!");
+    console.log(e);
     playSound(e);
   });
 });
 
 function playSound(e) {
-  const audio = $(`audio[data-key="${e.keyCode}"]`)[0];
-  const key = $(`div[data-key="${e.keyCode}"]`);
+  // console.log(e);
+  let audio = $(`audio[data-key="${e.keyCode}"]`)[0];
+  // if (e.type !== "keydown"){
+  //   audio = $(`audio[data-key="${e}"]`)[0];
+  // }
+  // console.log(audio);
+  // const key = $(`div[data-key="${e.keyCode}"]`);
   // if (!audio) return;
-  console.log(audio);
   audio.currentTime = 0;
   audio.play();
 }
+
+$(function() {
+  $('div .key').click(function(e) {
+    e.keyCode = $(this).attr('data-key');
+    console.log(e);
+    // console.log($(this).attr('data-key'));
+    playSound(e);
+    // playSound($('div .key').attr('data-key'));
+    // console.log($('div .key').attr('data-key'));
+
+  });
+});
