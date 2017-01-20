b2ff7535090b39f41d29d3f4d30d6b951922b690
Sung Kwang David Kim <sungkwangkim@gmail.com>
2017-01-20 14:42:30 -0800

    create clicked, pressed effect

--- js/jquery.sounds.js
+++ js/jquery.sounds.js
@@ -1,3 +1,5 @@
+let timeout = null;
+
 $(function(){
   $(window).keydown((e) => {
     playSound(e);
@@ -6,15 +8,40 @@ $(function(){
 
 function playSound(e) {
   let audio = $(`audio[data-key="${e.keyCode}"]`)[0];
+  // console.log(e);
+  let div = $(`div[data-key="${e.keyCode}"]`)[0];
+  // console.log(div);
+  $(div).addClass('playing');
+
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    $(div).removeClass('playing');
+  }, 100);
 
   audio.currentTime = 0;
   audio.play();
 }
 
+// $(function() {
+//   $('div .key').click(function(e) {
+//     e.keyCode = $(this).attr('data-key');
+//
+//     playSound(e);
+//   });
+// });
+
 $(function() {
   $('div .key').click(function(e) {
     e.keyCode = $(this).attr('data-key');
-
+    // $(this).addClass('playing');
     playSound(e);
   });
 });
+
+// $(function() {
+//   $('div .key').mouseup(function(e) {
+//     e.keyCode = $(this).attr('data-key');
+//     $(this).removeClass('playing');
+//     // playSound(e);
+//   });
+// });
